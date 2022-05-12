4431ba84251f8a6476dcfcb2ed8995767e5e5966
danieltolo1 <danieltolo1@hotmail.com>
2022-05-12 14:48:55 -0500

    header terminado y comienzo de trabajo con Javascrip

--- js/index.js
+++ js/index.js
@@ -1,9 +1,49 @@
 
+const sectionHotels = document.getElementById("main_card");
+var filterCountries = document.getElementById("filtercountries");
+
+
+filterCountries.addEventListener('change',
+  function(){
+    var selectedOption = this.options[filterCountries.selectedIndex];
+    console.log(selectedOption.value + ': ' + selectedOption.text);
+  });
+
+
+
+
+    
+
+
+
+
+
 
 async function llamarHoteles (){
     const response = await fetch('https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels');
     const data = await response.json();
-    console.log(data)
+    
+
+    data.forEach((element) => {
+       // console.log(element.name)
+        
+
+        
+        const cardHotel = document.createElement("div");
+        cardHotel.setAttribute("class", "card");
+
+        const imageHotel = document.createElement("img");
+        imageHotel.setAttribute("class", "card__image");
+        imageHotel.setAttribute("src", element.photo);
+        imageHotel.setAttribute("alt", "imagen del hotel " + element.name);
+
+        sectionHotels.appendChild(cardHotel);
+        cardHotel.appendChild(imageHotel);
+
+    });
+
+
+
 }
 
 llamarHoteles()
