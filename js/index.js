e4bcf63d89b4a0a74763ea1094ddfbd843e416bc
danieltolo1 <danieltolo1@hotmail.com>
2022-05-13 14:28:12 -0500

    renderizacion de la api por javaScrpit parte 1

--- js/index.js
+++ js/index.js
@@ -1,49 +1,62 @@
 
 const sectionHotels = document.getElementById("main_card");
-var filterCountries = document.getElementById("filtercountries");
-
-
-filterCountries.addEventListener('change',
-  function(){
-    var selectedOption = this.options[filterCountries.selectedIndex];
-    console.log(selectedOption.value + ': ' + selectedOption.text);
-  });
+const filterCountries = document.getElementById("filtercountries");
 
 
 
 
-    
 
-
-
-
+const elementoLimpieza = document.querySelector("main_card");
+      
 
 
 async function llamarHoteles (){
     const response = await fetch('https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels');
     const data = await response.json();
     
+    
+   
+    let filtros1 = data.filter((element) => {
+         
 
-    data.forEach((element) => {
-       // console.log(element.name)
-        
-
-        
-        const cardHotel = document.createElement("div");
-        cardHotel.setAttribute("class", "card");
-
-        const imageHotel = document.createElement("img");
-        imageHotel.setAttribute("class", "card__image");
-        imageHotel.setAttribute("src", element.photo);
-        imageHotel.setAttribute("alt", "imagen del hotel " + element.name);
-
-        sectionHotels.appendChild(cardHotel);
-        cardHotel.appendChild(imageHotel);
+        if (element.country === selectedOption.text){
+            //console.log("los paises son", element.name)
+            return element ;
+        } 
 
     });
 
 
+    filtros1.forEach((element) => {
 
+            let   cardHotel = document.createElement("div");
+            cardHotel.setAttribute("class", "card");
+            cardHotel.setAttribute("id", "cardid");
+            sectionHotels.appendChild(cardHotel);
+            const imageHotel = document.createElement("img");
+            imageHotel.setAttribute("class", "card__image"); 
+            imageHotel.setAttribute("src", element.photo);
+            imageHotel.setAttribute("alt", "imagen del hotel " + element.name);
+            cardHotel.appendChild(imageHotel);
+            
+            console.log(element)
+            
+        
+            //sectionHotels.removeChild(cardHotel);
+    });
+    
+    
 }
 
-llamarHoteles()
+
+
+
+filterCountries.addEventListener('change',
+  function(){
+    window.selectedOption = this.options[filterCountries.selectedIndex];
+    //console.log(selectedOption.text);
+    llamarHoteles()
+    
+  });
+
+
