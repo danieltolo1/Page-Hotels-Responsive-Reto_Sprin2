021998e244597310fb1f7a2b30585b304904eedb
danieltolo1 <danieltolo1@hotmail.com>
2022-05-18 16:46:45 -0500

    trabajando con los filtros para renderizar al dom

---
+++ js/data.js
@@ -0,0 +1,14 @@
+data[{createdAt:"2022-05-08T17:17:19.280Z",
+      name:"Entre Cielos",
+      avatar:"https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/741.jpg",
+      id:"1",
+      slug:"entre-cielos",
+      photo:"https://i.ibb.co/XZmZx0C/entre-cielos.jpg",
+      description:"Ubicado en una de las regiones vitivinícolas más grandes de América Latina, Entre Cielos fue pensado y construido en un campo de 8 hectáreas rodeado de viñedos malbec y una imponente vista de la Cordillera de Los Andes.",
+      availabilityFrom:1652485250121,
+      availabilityTo:1653349250121,
+      rooms:16,
+      city:"Mendoza",
+      country:"Argentina",
+      price:4
+    }]
--- js/index.js
+++ js/index.js
@@ -1,51 +1,66 @@
 
 const sectionHotels = document.getElementById("main_card");
 const filterCountries = document.getElementById("filtercountries");
+const filterPrice = document.getElementById("filter-Price");
 
 
-
-
-
-const elementoLimpieza = document.querySelector("main_card");
       
+// FUNCION PARA RECORRER LA DATA FILTRADA 
+
+function ForEachFiltrado (filtros1) {
+
+    filtros1.forEach((element) => {
+
+        let   cardHotel = document.createElement("div");
+        cardHotel.setAttribute("class", "card");
+        cardHotel.setAttribute("id", "cardid");
+        sectionHotels.appendChild(cardHotel);
+        const imageHotel = document.createElement("img");
+        imageHotel.setAttribute("class", "card__image"); 
+        imageHotel.setAttribute("src", element.photo);
+        imageHotel.setAttribute("alt", "imagen del hotel " + element.name);
+        cardHotel.appendChild(imageHotel);
+});
+}
+
+// FUNCION PARA RECORRER LA DATA COMPLETA
+
+function ForEachCompleta (data) {
+
+    data.forEach((element) => {
+        let   cardHotel = document.createElement("div");
+        cardHotel.setAttribute("class", "card");
+        cardHotel.setAttribute("id", "cardid");
+        sectionHotels.appendChild(cardHotel);
+        const imageHotel = document.createElement("img");
+        imageHotel.setAttribute("class", "card__image"); 
+        imageHotel.setAttribute("src", element.photo);
+        imageHotel.setAttribute("alt", "imagen del hotel " + element.name);
+        cardHotel.appendChild(imageHotel);
+});
+
+}
+
+
+
 
 
 async function llamarHoteles (){
     const response = await fetch('https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels');
     const data = await response.json();
     
-    
-   
-    let filtros1 = data.filter((element) => {
-         
 
-        if (element.country === selectedOption.text){
-            //console.log("los paises son", element.name)
+
+    let filtros1 = data.filter((element, index) => {
+               
+        if (element.country === selectedCountry.value  &&  element.price === parseInt(selectedPrice.value)){
             return element ;
         } 
-
     });
 
-
-    filtros1.forEach((element) => {
-
-            let   cardHotel = document.createElement("div");
-            cardHotel.setAttribute("class", "card");
-            cardHotel.setAttribute("id", "cardid");
-            sectionHotels.appendChild(cardHotel);
-            const imageHotel = document.createElement("img");
-            imageHotel.setAttribute("class", "card__image"); 
-            imageHotel.setAttribute("src", element.photo);
-            imageHotel.setAttribute("alt", "imagen del hotel " + element.name);
-            cardHotel.appendChild(imageHotel);
-            
-            console.log(element)
-            
-        
-            //sectionHotels.removeChild(cardHotel);
-    });
-    
-    
+    sectionHotels.innerHTML="";
+    ForEachFiltrado(filtros1); 
+      
 }
 
 
@@ -53,10 +68,15 @@ async function llamarHoteles (){
 
 filterCountries.addEventListener('change',
   function(){
-    window.selectedOption = this.options[filterCountries.selectedIndex];
-    //console.log(selectedOption.text);
+    window.selectedCountry = this.options[filterCountries.selectedIndex];
+    console.log(selectedCountry.value);
     llamarHoteles()
-    
   });
 
 
+filterPrice.addEventListener('change',
+  function(){
+      window.selectedPrice = this.options[filterPrice.selectedIndex];
+      //console.log(typeof(selectedPrice.value));
+      llamarHoteles()
+  });
